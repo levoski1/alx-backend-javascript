ee3cbf2027efbfc11fe6d15f5fc8a8f843835c8d
Ugwoke levi <soromtolevi1@gmail.com>
2024-08-13 11:39:00 +0100

    lint

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -3,11 +3,10 @@
  * @author Ugwoke levi <https://github.com/levoski1>
  * @returns {{id: Number, firstName: String, location: String}[]}
  */
-
 export default function getListStudents(){
-    return [
-        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-        { id: 2, firstName: 'James', location: 'Columbia' },
-        { id: 5, firstName: 'Serena', location: 'San Francisco' }
-      ]
+  return [
+    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+    { id: 2, firstName: 'James', location: 'Columbia' },
+    { id: 5, firstName: 'Serena', location: 'San Francisco' }
+  ];
 };
